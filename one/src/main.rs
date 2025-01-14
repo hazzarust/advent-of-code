d8a627698a4daaba984bc1348f3abee521f50200
hazzarust <itshazzag@gmail.com>
2025-01-14 14:19:40 +0000

    One: Part Two complete

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,4 +1,3 @@
-use std::fs::read_to_string;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -36,6 +35,51 @@ fn main() {
         }
     }
 
-    println!{"Left: {:?}, Right{:?}", left, right};
+    println!("{:?}", how_many_times(left, right));
+
+
+    
+}
+
+pub fn find_difference(left: Vec<u32>, right: Vec<u32>) -> Vec<u32>{
+    let vector: Vec<u32> = left.iter().zip(right.iter()).map(|x|{
+        let mut y = 0;
+        if x.0 > x.1{
+            y = x.0 - x.1;
+        }else if x.0 < x.1{
+            y = x.1 - x.0;
+        }else{
+        }
+        y
+    })
+    .collect();
+    
+    vector
+}
+
+pub fn total_distance(vector: Vec<u32>) -> u32{
+    let mut number = 0;
+    for x in vector{
+        number+= x;
+    }
+    number
+}
+
+pub fn how_many_times(left: Vec<u32>, right: Vec<u32>) -> u32{
+    let mut how_many: Vec<(u32, u32)> = vec![];
+    for (position, l) in left.iter().enumerate(){
+        how_many.push((*l, 0));
+        for r in &right{ 
+            if r == l{
+                how_many[position].1 += 1;
+            }
+        }
+    };
+
+    let mut number = 0;
+    for x in how_many.iter().map(|x| x.0 * x.1){
+        number+= x;
+    }
+    number
 }
 
